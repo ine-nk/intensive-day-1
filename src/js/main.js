2b989814737ef7d075d4ecf2acf9f5e111dc07ac
ine-nk <its.nvkz@gmail.com>
2020-01-25 18:30:17 +0700

    added gulp and scss

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// alert('Hi!!')
+// function loadCSS(hf) {
+//     var ms=document.createElement("link");ms.rel="stylesheet";
+//     ms.href=hf;document.getElementsByTagName("head")[0].appendChild(ms);
+//   }
+//   loadCSS("css/header.min.css"); //Header Styles (compress & paste to header after release)
+//   loadCSS("css/main.min.css");   //User Styles + Media Queries
